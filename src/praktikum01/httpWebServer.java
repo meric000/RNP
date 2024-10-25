3eed27bb60a232267777e5311b2751cf4d024f80
Meric Demir <meric.demir@haw-hamburg.de>
2024-10-25 13:23:07 +0200

    anfang webserver

--- src/praktikum01/httpWebServer.java
+++ src/praktikum01/httpWebServer.java
@@ -13,7 +13,7 @@ import java.net.InetSocketAddress;
 public class httpWebServer {
     public static void main(String[] args) throws IOException {
         //Create Server
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         //Create a context for a specific path and set the default executor
         server.createContext("/", new Myhandler());
 
@@ -21,7 +21,7 @@ public class httpWebServer {
         server.setExecutor(null);
         server.start();
 
-        System.out.println("Server is running on port 8000");
+        System.out.println("Server is running on port 8080");
     }
 }
 
@@ -29,28 +29,30 @@ class Myhandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        File htmlFile = new File("/index.html");
+        File htmlFile = new File("C:\\Users\\meric\\OneDrive\\Desktop\\UNI\\Sem6\\RNP\\src\\praktikum01\\Testweb\\index.html");
 
 
 
         if (htmlFile.exists()) {
-            exchange.getRequestHeaders().set("Content-Type", "text/html; charset=UTF-8");
-            exchange.sendResponseHeaders(200,htmlFile.length());
+            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
+            exchange.sendResponseHeaders(200, htmlFile.length());
+
             try (FileInputStream fis = new FileInputStream(htmlFile);
-                 OutputStream os = exchange.getResponseBody()){
+                 OutputStream os = exchange.getResponseBody()) {
                 byte[] buffer = new byte[4096];
                 int bytesRead;
                 while ((bytesRead = fis.read(buffer)) > 0) {
                     os.write(buffer, 0, bytesRead);
                 }
             }
-
-        }else {
+        } else {
             String errorResponse = "404 (Not Found)\nDie angeforderte Datei wurde nicht gefunden.";
+            exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=UTF-8");
             exchange.sendResponseHeaders(404, errorResponse.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(errorResponse.getBytes());
-            os.close();
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(errorResponse.getBytes());
+            }
         }
+
     }
 }
